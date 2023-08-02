4b686ae9cbb72dad60f001d278b6e3b2ce40a9ac
Hebilicious <xsh4k3@gmail.com>
2023-08-02 19:49:35 +0700

    chore(release): v0.0.9

--- action/index.cjs
+++ action/index.cjs
@@ -9825,10 +9825,58 @@ const node_fs = __nccwpck_require__(7561);
 const node_path = __nccwpck_require__(9411);
 const node_url = __nccwpck_require__(1041);
 
+const message = (
+  /* md */
+  `We cannot recreate the issue with the provided information. **Please add a reproduction in order for us to be able to investigate.**
+
+### **Why was this issue marked with the \`needs-reproduction\` label?**
+
+To be able to investigate, we need access to a reproduction to identify what triggered the issue. Please provide a link to either :
+
+- a public GitHub repository
+- a [CodeSandbox](https://codesandbox.io/)
+- a [StackBlitz](https://stackblitz.com/).
+
+To make sure the issue is resolved as quickly as possible, please make sure that the reproduction is as **minimal** as possible. This means that you should **remove unnecessary code, files, and dependencies** that do not contribute to the issue.
+
+### **I added a link, why was it still marked?**
+
+Ensure the link is pointing to a codebase that is accessible (e.g. not a private repository). "[example.com](http://example.com/)", "n/a", "will add later", etc. are not acceptable links -- we need to see a public codebase. See the above section for accepted links.
+
+### **What happens if I don't provide a sufficient minimal reproduction?**
+
+Issues with the \`needs-reproduction\` label that receives no meaningful activity (e.g. new comments with a reproduction link) will be closed by the maintainers and won't be looked at.
+
+### **I did not open this issue, but it is relevant to me, what can I do to help?**
+
+Anyone experiencing the same issue is welcome to provide a minimal reproduction following the above steps. Furthermore, you can upvote the issue using a reaction on the topmost comment.
+
+- Please **do not** comment "I have the same issue" without repro steps
+- Please **do not** comment un-helpful messages such as "+1" on an issue, use reactions on the first comment instead.
+
+### **I think my reproduction is good enough, why aren't you looking into it quicker?**
+
+We constantly monitor open issues for new comments.
+
+However, sometimes we might miss one or two. We apologize, and kindly ask you to refrain from tagging core maintainers, as that will usually not result in increased priority.
+
+Upvoting issues to show your interest will help us prioritize and address them as quickly as possible.
+
+### **I have a minimal reproduction to contribute to a closed issue.**
+
+If you have a minimal reproduction for a closed issue, please write a comment on the issue with a link to the reproduction.
+We will review it and reopen the issue if it is sufficient.
+
+### **Useful Resources**
+
+- [Why reproductions are required](https://antfu.me/posts/why-reproductions-are-required)
+- [How to create a Minimal, Complete, and Verifiable example](https://stackoverflow.com/help/mcve)`
+);
+
 const actionName = "reproduire";
 const __filename$1 = node_url.fileURLToPath((typeof document === 'undefined' ? (__nccwpck_require__(7310).pathToFileURL)(__filename).href : (document.currentScript && document.currentScript.src || new URL('index.cjs', document.baseURI).href)));
-const __dirname$1 = node_path.dirname(__filename$1);
-const coreLog = (message) => core.info(`[${actionName}]: ${message}`);
+node_path.dirname(__filename$1);
+const coreLog = (message2) => core.info(`[${actionName}]: ${message2}`);
 const reproductionLabel = core.getInput("label") ?? "needs-reproduction";
 function getClient() {
   const token = core.getInput("repo-token") ?? process.env.GITHUB_TOKEN;
@@ -9836,10 +9884,9 @@ function getClient() {
 }
 function getMessage() {
   const userPath = node_path.join(process.env.GITHUB_WORKSPACE ?? "", ".github", actionName, "needs-reproduction.md");
-  const defaultPath = node_path.join(__dirname$1, "./messages/needs-reproductions.md");
   if (node_fs.existsSync(userPath))
     return node_fs.readFileSync(userPath, "utf8");
-  return node_fs.readFileSync(defaultPath, "utf8");
+  return message;
 }
 
 async function main() {
